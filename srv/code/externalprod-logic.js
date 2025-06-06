0bb11dbf27e4775eae54da665be9aaffcec9da42
Carlos Ribeiro <carloscde@gmail.com>
2025-06-06 13:32:04 +0000

    update hybrid call local

---
+++ srv/code/externalprod-logic.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @On(event = { "READ" }, entity = "cap_ext_servService.ExternalProd")
+ * @param {cds.Request} request - User information, tenant-specific CDS model, headers and query parameters
+ * @param {Function} next - Callback function to the next handler
+*/
+module.exports = async function(request, next) {
+    // Connect to the external Northwind service
+    const northwindService = await cds.connect.to('ODataWebV4.Northwind.Model');
+
+    // Get the external entity identifier for Products
+    const { Products } = northwindService.entities;
+
+    // Fetch data from the external Northwind Products entity
+    const externalProducts = await northwindService.run(SELECT.from(Products));
+
+    // If externalProducts is undefined, return an empty array
+    if (!externalProducts) {
+        return [];
+    }
+
+    // Return the fetched data
+    return externalProducts;
+}
